c674b8721c331ac2d0a35bf8eca6f15e8e21c4a4
Bhavadharani <bhavadharanik412@gmail.com>
2025-09-04 10:57:55 +0530

    Update gcd.java

--- gcd.java
+++ gcd.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import java.math.BigInteger;
-public class gcd{
+public class Main{
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
@@ -14,8 +14,12 @@ public class gcd{
 		System.out.println("Space complexity (Auxiliary Space) :O(log(min(a,b))");
 		int builtin = BigInteger.valueOf(a).gcd(BigInteger.valueOf(b)).intValue();
 		System.out.println("gcd using builtin : "+builtin);
-		System.out.println("Time complexity : O(min(a, b))");
-		System.out.println("Space complexity : O(1) Space");
+		System.out.println("Time complexity : O(log(min(a, b)))");
+		System.out.println("Space complexity : O(1)");
+		int loop_euclid = gcd_Euclidean_iterative(a,b);
+		System.out.println("gcd using loop with implementation of euclid : "+loop_euclid);
+		System.out.println("Time complexity : O(log(min(a, b)))");
+		System.out.println("Space complexity : O(1)");
 		
 	}
 	static int gcd(int a, int b){
@@ -30,5 +34,13 @@ public class gcd{
 	}
     static int gcd_Euclidean(int a, int b) {
         return (b == 0) ? a : gcd(b, a % b);
+    }
+    static int gcd_Euclidean_iterative(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
     }
 }
