dbefcf5aef495a2a94716652c2991821fa8480ce
Bhavadharani <bhavadharanik412@gmail.com>
2025-10-31 11:02:21 +0530

    Implement reverse linked list function

---
+++ reverse_LinkedList.java
@@ -0,0 +1,12 @@
+public class Solution{
+    public static Node reverseLinkedList(Node head){
+        Node curr = head, prev = null, next;
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
